9715347ce57c10ea676a7bc79269d378b840db7e
Technote <technote.space@gmail.com>
2019-05-04 15:32:29 +0900

    simplify

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,1 @@
-[...Array(150).keys()].forEach(i => ++i && console.log(('00' + i + (0 === i % 3 || 3 === i % 10 ? "!" : "")).substr(i >= 100 ? 2 : -3, i >= 100 ? 3 : undefined)));
+[...Array(150).keys()].forEach(i => ++i && console.log(('00' + (i < 100 && (0 === i % 3 || 3 === i % 10) ? i + "!" : i)).substr(-3)));
